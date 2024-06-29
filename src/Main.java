e48a029dc7e4080c36ed12949f783d2fa1e2723e
yashika848 <yashikatomar341@gmail.com>
2024-06-29 17:46:56 +0530

    Initial comment for this project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
-public class Main {
+import java.util.Scanner;
+class guessinggame {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int n = 6;
+        int i, guess;
+        Scanner Sc = new Scanner(System.in);
+        int number = (int) (100 * Math.random());
+        System.out.println("Enter a number between 1 and 100." + "Guess the the number within 6 attempts");
+        for (i = 0; i < n; i++) {
+            System.out.println("Guess the number");
+            guess = Sc.nextInt();
+            if (guess == number) {
+                System.out.println(" hey cutie congratulation " + "you guessed the number. ");
+
+            } else if (guess < number && i != n - 1) {
+                System.out.println(" The number is Greater than" + " guess");
+            } else if (guess > number && i != n - 1) {
+                System.out.println("The number is less than" + " guess");
+            }
+        }
+
+        if (i == n) {
+            System.out.println(" Game Over " + " you use all your attempts ");
+            System.out.println(" The number which has to be guessed is" + "=" + number);
+        }
+            System.out.println(" Thanks for playing ........");
+
+        }
     }
-}
+
+
+
+
+
+
+
+
+
---
+++ src/task2.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class task2 {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the marks of maths");
+        int a =sc.nextInt();
+        System.out.println("Enter the marks of english");
+        int b = sc.nextInt();
+        System.out.println("Enter the marks of physics");
+        int c=sc.nextInt();
+        System.out.println("Enter the marks of chemistry");
+        int d= sc.nextInt();
+        System.out.println("Enter the marks of biology");
+        int e = sc.nextInt();
+
+        int sum = (a+b+c+d+e);
+        System.out.println("The sum of the marks is"+"="+sum);
+        int percentage= (sum)/5;
+        System.out.println("The percentage of subject is"+"="+percentage+"%");
+          if(percentage>=90 && percentage<=100)
+        {
+            System.out.println("your Grade is Excellent" + "=" + "A+");
+        }  else if(percentage>=70 && percentage<=89){
+            System.out.println("your Grade is very good"+"="+"B+");
+
+        }else if(percentage >=50 && percentage<=69){
+            System.out.println("your Grade is Good"+"="+"C+");
+        } else if( percentage >=30 && percentage<=49)
+        {
+            System.out.println("your Grade is not good you have to work hard" + "=" + "D+");
+        }
+         else if(percentage>=0 && percentage<=29){
+
+            System.out.println(" your are fail in your exam" + "=" + "E");
+        }else{
+            System.out.println(" invalid marks");
+        }
+
+    }
+}
+
