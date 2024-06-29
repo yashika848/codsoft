fec1f7fc2de7495782482391d10d088ca65e8085
yashika848 <yashikatomar341@gmail.com>
2024-06-29 19:55:15 +0530

    this is the java projects

--- src/task2.java
+++ src/task2.java
@@ -1,26 +1,25 @@
 import java.util.Scanner;
-public class task2 {
-    public static void main(String[]args){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the marks of maths");
-        int a =sc.nextInt();
-        System.out.println("Enter the marks of english");
-        int b = sc.nextInt();
-        System.out.println("Enter the marks of physics");
-        int c=sc.nextInt();
-        System.out.println("Enter the marks of chemistry");
-        int d= sc.nextInt();
-        System.out.println("Enter the marks of biology");
-        int e = sc.nextInt();
 
-        int sum = (a+b+c+d+e);
-        System.out.println("The sum of the marks is"+"="+sum);
-        int percentage= (sum)/5;
-        System.out.println("The percentage of subject is"+"="+percentage+"%");
-          if(percentage>=90 && percentage<=100)
-        {
+            public class task2 {
+                public static void main(String[] args) {
+                    Scanner sc = new Scanner(System.in);
+                    System.out.println("Enter the marks of maths");
+                    int a = sc.nextInt();
+                    System.out.println("Enter the marks of english");
+                    int b = sc.nextInt();
+                    System.out.println("Enter the marks of physics");
+                    int c = sc.nextInt();
+                    System.out.println("Enter the marks of chemistry");
+                    int d = sc.nextInt();
+                    System.out.println("Enter the marks of biology");
+                    int e = sc.nextInt();
+
+                    int sum = (a + b + c + d + e);
+                    System.out.println("The sum of the marks is" + "=" + sum);
+                    int percentage = (sum) / 5;
             System.out.println("your Grade is Excellent" + "=" + "A+");
-        }  else if(percentage>=70 && percentage<=89){
+        }
+
             System.out.println("your Grade is very good"+"="+"B+");
 
         }else if(percentage >=50 && percentage<=69){
